d24792cf4ae804963d77e1558383589394a29928
utopusc <87100762+utopusc@users.noreply.github.com>
2023-05-25 16:16:47 +0000

    homepg

---
+++ .next/static/webpack/pages/index.91543221b6572ac9.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/esm/defineProperty.js":
+/*!*************************************************************************************!*\
+  !*** ./node_modules/next/node_modules/@babel/runtime/helpers/esm/defineProperty.js ***!
+  \*************************************************************************************/
+/***/ (function(__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": function() { return /* binding */ _defineProperty; }\n/* harmony export */ });\nfunction _defineProperty(obj, key, value) {\n  if (key in obj) {\n    Object.defineProperty(obj, key, {\n      value: value,\n      enumerable: true,\n      configurable: true,\n      writable: true\n    });\n  } else {\n    obj[key] = value;\n  }\n\n  return obj;\n}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9ub2RlX21vZHVsZXMvbmV4dC9ub2RlX21vZHVsZXMvQGJhYmVsL3J1bnRpbWUvaGVscGVycy9lc20vZGVmaW5lUHJvcGVydHkuanMuanMiLCJtYXBwaW5ncyI6Ijs7OztBQUFlO0FBQ2Y7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EsS0FBSztBQUNMLElBQUk7QUFDSjtBQUNBOztBQUVBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vbm9kZV9tb2R1bGVzL25leHQvbm9kZV9tb2R1bGVzL0BiYWJlbC9ydW50aW1lL2hlbHBlcnMvZXNtL2RlZmluZVByb3BlcnR5LmpzPzY3MTMiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gX2RlZmluZVByb3BlcnR5KG9iaiwga2V5LCB2YWx1ZSkge1xuICBpZiAoa2V5IGluIG9iaikge1xuICAgIE9iamVjdC5kZWZpbmVQcm9wZXJ0eShvYmosIGtleSwge1xuICAgICAgdmFsdWU6IHZhbHVlLFxuICAgICAgZW51bWVyYWJsZTogdHJ1ZSxcbiAgICAgIGNvbmZpZ3VyYWJsZTogdHJ1ZSxcbiAgICAgIHdyaXRhYmxlOiB0cnVlXG4gICAgfSk7XG4gIH0gZWxzZSB7XG4gICAgb2JqW2tleV0gPSB2YWx1ZTtcbiAgfVxuXG4gIHJldHVybiBvYmo7XG59Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./node_modules/next/node_modules/@babel/runtime/helpers/esm/defineProperty.js\n");
+
+/***/ })
+
+});
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,17 +5,69 @@ import Cursor from "../components/cursor";
 import ScrollToTop from "../components/scrollToTop";
 import LoadingScreen from "../components/Loading-Screen";
 import "../styles/main.scss";
+import LightTheme from "../layouts/Light";
+import Team3 from "../components/Team3/team3";
+import Intro5 from "../components/Intro5/intro5";
+import AboutUs6 from "../components/About-us6/about-us6";
+import Services6 from "../components/Services6/services6";
+import CaseStudy from "../components/Case-study/case-study";
+import NavbarArch from "../components/Navbar-arch/navbar-arch";
+import LatestNews from "../components/Latest-news/latest-news";
+import WorksStyle5 from "../components/Works-style5/works-style5";
+import FooterArch from "../components/Footer-arch/footer-arch.jsx";
+import ContactArch from "../components/Contact-arch/contact-arch";
+import TestimonialsArch from "../components/Testimonials-arch/testimonials-arch";
 
-function MyApp({ Component, pageProps }) {
+
+const MyApp = () => {
+  const navbarRef = React.useRef(null);
+  React.useEffect(() => {
+    var navbar = navbarRef.current;
+    if (window.pageYOffset > 300) {
+      navbar.classList.add("nav-scroll");
+    } else {
+      navbar.classList.remove("nav-scroll");
+    }
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        navbar.classList.add("nav-scroll");
+      } else {
+        navbar.classList.remove("nav-scroll");
+      }
+    });
+  }, [navbarRef]);
   return (
     <>
+      <Head>
+        <link rel="stylesheet" defer href="/css/arch-skin-light.css" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Khand:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Head>
         <title>Vie</title>
         <link rel="icon" href="/img/favicon.ico" />
       </Head>
       <Cursor />
       <LoadingScreen />
-      <Component {...pageProps} />
+      <LightTheme>
+        <NavbarArch navbarRef={navbarRef} theme="themeL" />
+        <Intro5 />
+        <AboutUs6 />
+        <Services6 />
+        <WorksStyle5 />
+        <CaseStudy />
+        <Team3 />
+        <TestimonialsArch />
+        <LatestNews />
+        <ContactArch />
+        <FooterArch />
+      </LightTheme>
       <ScrollToTop />
       <Script id="wow" src="/js/wow.min.js"></Script>
       <Script
@@ -32,6 +84,7 @@ function MyApp({ Component, pageProps }) {
       <Script id="wowInit" strategy="lazyOnload">{`new WOW().init();`}</Script>
     </>
   );
-}
+};
 
 export default MyApp;
+
