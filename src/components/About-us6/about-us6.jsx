ab628513f96e71486bef8a4a4f579a32660caf70
utopusc <87100762+utopusc@users.noreply.github.com>
2023-05-30 21:44:28 +0000

    aboutbitti

--- src/components/About-us6/about-us6.jsx
+++ src/components/About-us6/about-us6.jsx
@@ -27,15 +27,14 @@ const AboutUs6 = () => {
             </div>
 
             <div className="col-lg-6 valign">
-  <div className="content">
-    <h6 className="sub-title main-color ls10 text-u">About Us</h6>
-    <h3>Experience You Can Trust</h3>
-    <p>
-      At PaintMeDesign, we pride ourselves on our rich history of delivering high-quality home improvement services in Los Angeles. With 15 years of experience in the industry, we've mastered the art of exceeding our clients' expectations. Our skilled team is proficient in a broad range of services, including General Mounting, Furniture Assembly, Help Moving, Furniture Removal, Cleaning, Minor Home Repairs, Electrical Help, Plumbing, and Heavy Lifting & Loading. We're more than just a service provider, we're a partner committed to enhancing the comfort and value of your home. 
-    </p>
-  </div>
-</div>
-
+              <div className="content">
+                <h6 className="sub-title main-color ls10 text-u">About Us</h6>
+                <h3>Experience You Can Trust</h3>
+                <p>
+                  At PaintMeDesign, we pride ourselves on our rich history of delivering high-quality home improvement services in Los Angeles. With 15 years of experience in the industry, we ve mastered the art of exceeding our clients expectations. Our skilled team is proficient in a broad range of services, including General Mounting, Furniture Assembly, Help Moving, Furniture Removal, Cleaning, Minor Home Repairs, Electrical Help, Plumbing, and Heavy Lifting & Loading. We re more than just a service provider, we re a partner committed to enhancing the comfort and value of your home.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
