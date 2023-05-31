ae9e98bc69a75e11f98d060ad0838918947b18e0
utopusc <87100762+utopusc@users.noreply.github.com>
2023-05-31 00:29:13 +0000

    portfolio

--- src/components/Works-style5/works-style5.jsx
+++ src/components/Works-style5/works-style5.jsx
@@ -16,7 +16,7 @@ const WorksStyle5 = () => {
           <div className="col-lg-8 col-md-10">
             <div className="sec-head  text-center">
               <h2 className="wow fadeIn" data-wow-delay=".3s">
-                Projects
+                Portfolio
               </h2>
             </div>
           </div>
@@ -24,141 +24,71 @@ const WorksStyle5 = () => {
       </div>
       <div className="container">
         <div className="row">
-          <div className="filtering col-12">
-            <div className="filter custom-font wow fadeIn" data-wow-delay=".5s">
-              <span data-filter="*" className="active">
-                All
-              </span>
-              <span data-filter=".brand">Interior</span>
-              <span data-filter=".web">Architecture</span>
-              <span data-filter=".graphic">Residential</span>
-            </div>
-          </div>
-
           <div className="gallery full-width">
             <div
-              className="col-md-6 items graphic lg-mr wow fadeInUp"
+              className="col-md-6 items web wow fadeInUp"
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
                   <a className="imago wow">
-                    <img src="/img/arch/work/1.jpg" alt="image" />
+                    <img src="/img/arch/work/1.png" alt="image" />
                     <div className="item-img-overlay"></div>
                   </a>
-                </Link>
-              </div>
-              <div className="cont">
-                <h5>BUGANVILLA HOUSE</h5>
-                <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Arch</Link>,
-                  <Link href="/works/works-dark">Interior</Link>
-                </span>
               </div>
             </div>
-
             <div
               className="col-md-6 items web wow fadeInUp"
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
                   <a className="imago wow">
-                    <img src="/img/arch/work/2.jpg" alt="image" />
+                    <img src="/img/arch/work/2.png" alt="image" />
                     <div className="item-img-overlay"></div>
                   </a>
-                </Link>
-              </div>
-              <div className="cont">
-                <h5>The Concept</h5>
-                <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Arch</Link>,
-                  <Link href="/works/works-dark">Interior</Link>
-                </span>
               </div>
             </div>
-
             <div
               className="col-md-6 items web wow fadeInUp"
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
                   <a className="imago wow">
-                    <img src="/img/arch/work/5.jpg" alt="image" />
+                    <img src="/img/arch/work/3.png" alt="image" />
                     <div className="item-img-overlay"></div>
                   </a>
-                </Link>
-              </div>
-              <div className="cont">
-                <h5>Private House</h5>
-                <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Arch</Link>,
-                  <Link href="/works/works-dark">Interior</Link>
-                </span>
               </div>
             </div>
-
             <div
-              className="col-md-6 items web graphic wow fadeInUp"
+              className="col-md-6 items web wow fadeInUp"
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
                   <a className="imago wow">
-                    <img src="/img/arch/work/3.jpg" alt="image" />
+                    <img src="/img/arch/work/4.png" alt="image" />
                     <div className="item-img-overlay"></div>
                   </a>
-                </Link>
-              </div>
-              <div className="cont">
-                <h5>Floating House Messinia</h5>
-                <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Arch</Link>,
-                  <Link href="/works/works-dark">Interior</Link>
-                </span>
               </div>
             </div>
-
             <div
-              className="col-md-6 items brand wow fadeInUp"
+              className="col-md-6 items web wow fadeInUp"
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
                   <a className="imago wow">
-                    <img src="/img/arch/work/4.jpg" alt="image" />
+                    <img src="/img/arch/work/5.png" alt="image" />
                     <div className="item-img-overlay"></div>
                   </a>
-                </Link>
-              </div>
-              <div className="cont">
-                <h5>IN THE PINE FOREST</h5>
-                <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Arch</Link>,
-                  <Link href="/works/works-dark">Interior</Link>
-                </span>
               </div>
             </div>
-
             <div
-              className="col-md-6 items brand wow fadeInUp"
+              className="col-md-6 items web wow fadeInUp"
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
                   <a className="imago wow">
-                    <img src="/img/arch/work/6.jpg" alt="image" />
+                    <img src="/img/arch/work/6.png" alt="image" />
                     <div className="item-img-overlay"></div>
                   </a>
-                </Link>
-              </div>
-              <div className="cont">
-                <h5>VILLAS IN SOCHI</h5>
-                <span className="tags main-color custom-font">
-                  <Link href="/works/works-dark">Arch</Link>,
-                  <Link href="/works/works-dark">Interior</Link>
-                </span>
               </div>
             </div>
           </div>
